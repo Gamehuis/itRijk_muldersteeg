1c9be8abfcedc19a548eb25aff5fe0ad06aef57f
Gamehuis <109477463+Gamehuis@users.noreply.github.com>
2023-01-20 15:40:41 +0100

    V1

---
+++ CL1G6Vraagen/src/script.js
@@ -0,0 +1,19 @@
+const message = 'Vraag1' // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+// Log to console
+console.log(message)
+
+function Ant1Vraag1() {
+ window.open("https://antwoord29105719207069709217509712749102571297091475702915709.playcode.io")
+  }
+  
+function AntwVraag1() {
+  var vraag1 = prompt("select", "Antwoord");
+  if (vraag1 === "1") {
+  alert("Goed")
+  
+  }
+  }
